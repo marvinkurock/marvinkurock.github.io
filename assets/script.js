b43178af4ebd6103b9c51bfd84257363c5643ddc
Marvin <marvin.kurock@tech-excellence.de>
2023-12-05 15:51:02 +0100

    update

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ document.body.onload = (arguments) => {
       behavior: "smooth",
       block: "start",
     });
+    return false;
   };
   const wrapper = document.getElementById("wrapper");
   var isOnWelcome = true;
@@ -16,16 +17,28 @@ document.body.onload = (arguments) => {
         console.log("Scrolled past welcome section!");
         console.log(document.getElementsByTagName("nav"));
         document.getElementsByTagName("nav")[0].classList.add("inverted");
+        document.getElementById("nav-bg").classList.add("nav-bg-colored");
       }
     } else {
       if (!isOnWelcome) {
         isOnWelcome = true;
         console.log("Scrolled back to welcome section!");
         document.getElementsByTagName("nav")[0].classList.remove("inverted");
+        document.getElementById("nav-bg").classList.remove("nav-bg-colored");
       }
     }
   };
-  particlesJS.load("particles", "assets/particles.json", function () {
-    console.log("callback - particles.js config loaded");
-  });
+  if (
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i)
+  ) {
+    particlesJS.load("particles", "assets/mobile.json", function () {
+      console.log("callback - particles.js config loaded (mobile)");
+    });
+  } else {
+    particlesJS.load("particles", "assets/particles.json", function () {
+      console.log("callback - particles.js config loaded");
+    });
+  }
 };
